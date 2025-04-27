246ece9d21e7212a564cb441aae92beaf7ef0cfe
ZarachoAlan <alandavidzaracho@gmail.com>
2025-04-27 17:15:18 -0300

    cambios minimos y inserccion de JS para movimiento de tarjetas y arreglo de icono de la pagina

--- darkmode.js
+++ darkmode.js
@@ -10,3 +10,29 @@ toggleButton.addEventListener("click", () => {
     toggleButton.textContent = "🌙 Modo Oscuro";
   }
 });
+
+
+// animacion para las tarjetas
+let posicion = 0;
+
+function moverCarrusel(direccion) {
+  const track = document.querySelector('.carousel-track');
+  const totalCards = document.querySelectorAll('.carousel-track .card').length;
+
+  if (direccion === 1) {
+    // Botón derecha
+    posicion++;
+    if (posicion >= totalCards) {
+      posicion = 0; // vuelve al inicio
+    }
+  } else if (direccion === -1) {
+    // Botón izquierda
+    posicion--;
+    if (posicion < 0) {
+      posicion = totalCards - 1; // va al final
+    }
+  }
+
+  const desplazamiento = -(posicion * 100);
+  track.style.transform = `translateX(${desplazamiento}%)`;
+}
